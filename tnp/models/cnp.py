9f82b582bd160eb8f0787adc1461c56e28361300
MattAshman <matthew.ashman97@gmail.com>
2024-10-15 14:41:07 +0100

    Updated CNP implementation

--- tnp/models/cnp.py
+++ tnp/models/cnp.py
@@ -5,15 +5,18 @@ from torch import nn
 
 from ..networks.deepset import DeepSet
 from .base import ConditionalNeuralProcess
+from .tnp import TNPDecoder
 
 
 class CNPEncoder(nn.Module):
     def __init__(
         self,
         deepset: DeepSet,
+        x_encoder: nn.Module = nn.Identity(),
     ):
         super().__init__()
         self.deepset = deepset
+        self.x_encoder = x_encoder
 
     @check_shapes(
         "xc: [m, nc, dx]",
@@ -24,32 +27,26 @@ class CNPEncoder(nn.Module):
     def forward(
         self, xc: torch.Tensor, yc: torch.Tensor, xt: torch.Tensor
     ) -> torch.Tensor:
-        zc = self.deepset(xc, yc)
+        x = torch.cat((xc, xt), dim=1)
+        x_encoded = self.x_encoder(x)
+        xc_encoded, xt_encoded = x_encoded.split((xc.shape[1], xt.shape[1]), dim=1)
+
+        zc = self.deepset(xc_encoded, yc)
+
         # Use same context representation for every target point.
         zc = einops.repeat(zc, "m d -> m n d", n=xt.shape[-2])
+
+        # Concatenate xt to zc.
+        zc = torch.cat((zc, xt_encoded), dim=-1)
+
         return zc
-
-
-class CNPDecoder(nn.Module):
-    def __init__(self, z_decoder: nn.Module):
-        super().__init__()
-
-        self.z_decoder = z_decoder
-
-    @check_shapes(
-        "zc: [m, nt, dz]",
-        "xt: [m, nt, dx]",
-        "return: [m, nt, .]",
-    )
-    def forward(self, zc: torch.Tensor, xt: torch.Tensor) -> torch.Tensor:
-        return self.z_decoder(torch.cat((zc, xt), dim=-1))
 
 
 class CNP(ConditionalNeuralProcess):
     def __init__(
         self,
         encoder: CNPEncoder,
-        decoder: CNPDecoder,
+        decoder: TNPDecoder,
         likelihood: nn.Module,
     ):
         super().__init__(encoder, decoder, likelihood)
