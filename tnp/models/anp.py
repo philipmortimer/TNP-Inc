6e65a231e1b6acfb9e90dcb7269606a42adc6531
MattAshman <matthew.ashman97@gmail.com>
2024-10-15 14:40:30 +0100

    Updated ANP implementatioN

--- tnp/models/anp.py
+++ tnp/models/anp.py
@@ -14,12 +14,16 @@ class ANPEncoder(nn.Module):
         transformer_encoder: TransformerEncoder,
         mha_layer: MultiHeadAttentionLayer,
         xy_encoder: TransformerEncoder,
+        x_encoder: nn.Module = nn.Identity(),
+        y_encoder: nn.Module = nn.Identity(),
     ):
         super().__init__()
 
         self.transformer_encoder = transformer_encoder
         self.mha_layer = mha_layer
         self.xy_encoder = xy_encoder
+        self.x_encoder = x_encoder
+        self.y_encoder = y_encoder
 
     @check_shapes(
         "xc: [m, nc, dx]", "yc: [m, nc, dy]", "xt: [m, nt, dx]", "return: [m, n, dz]"
@@ -27,14 +31,20 @@ class ANPEncoder(nn.Module):
     def forward(
         self, xc: torch.Tensor, yc: torch.Tensor, xt: torch.Tensor
     ) -> torch.Tensor:
-        zc = torch.cat((xc, yc), dim=-1)
+        x = torch.cat((xc, xt), dim=1)
+        x_encoded = self.x_encoder(x)
+        xc_encoded, xt_encoded = x_encoded.split((xc.shape[1], xt.shape[1]), dim=1)
+
+        yc_encoded = self.y_encoder(yc)
+
+        zc = torch.cat((xc_encoded, yc_encoded), dim=-1)
         zc = self.xy_encoder(zc)
 
         # Self-attention layers on context tokens.
         zc = self.transformer_encoder(zc)
 
         # Cross-attention layer with input locations as query/keys.
-        zt = self.mha_layer(xt, xc, zc)
+        zt = self.mha_layer(xt_encoded, xc_encoded, zc)
 
         return zt
 
