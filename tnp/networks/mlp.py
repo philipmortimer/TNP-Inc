381b6c041f4553eaf84fd6b05756292419978f03
MattAshman <matthew.ashman97@gmail.com>
2024-10-15 14:46:41 +0100

    Added MLPWithEmbedding for embedding prior to MLP

--- tnp/networks/mlp.py
+++ tnp/networks/mlp.py
@@ -1,9 +1,11 @@
-from typing import Optional, Tuple
+from collections.abc import Iterable
+from typing import List, Optional, Tuple, Union
 
 import torch
 from torch import nn
 
 from ..utils.batch import compress_batch_dimensions
+from .embeddings import Embedding
 
 
 class MLP(nn.Module):
@@ -30,7 +32,6 @@ class MLP(nn.Module):
         num_layers: Optional[int] = None,
         width: Optional[int] = None,
         nonlinearity: Optional[nn.Module] = None,
-        dtype: Optional[torch.dtype] = None,
     ):
         super().__init__()
 
@@ -47,18 +48,52 @@ class MLP(nn.Module):
 
         # Build layers.
         if len(layers) == 0:
-            self.net = nn.Linear(in_dim, out_dim, dtype=dtype)
+            self.net = nn.Linear(in_dim, out_dim)
         else:
-            net = [nn.Linear(in_dim, layers[0], dtype=dtype)]
+            net = [nn.Linear(in_dim, layers[0])]
             for i in range(1, len(layers)):
                 net.append(nonlinearity)
-                net.append(nn.Linear(layers[i - 1], layers[i], dtype=dtype))
+                net.append(nn.Linear(layers[i - 1], layers[i]))
             net.append(nonlinearity)
-            net.append(nn.Linear(layers[-1], out_dim, dtype=dtype))
+            net.append(nn.Linear(layers[-1], out_dim))
             self.net = nn.Sequential(*net)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x, uncompress = compress_batch_dimensions(x, 2)
+        x, uncompress = compress_batch_dimensions(x, 1)
         x = self.net(x)
         x = uncompress(x)
         return x
+
+
+class MLPWithEmbedding(MLP):
+    def __init__(
+        self,
+        embeddings: Union[Embedding, Tuple[Embedding, ...]],
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        if not isinstance(embeddings, Iterable):
+            embeddings = (embeddings,)
+
+        self.embeddings = nn.ModuleList(embeddings)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+
+        # Embed the input.
+        embedded_dims: List[int] = []
+        embeddings: List[torch.Tensor] = []
+        for embedding in self.embeddings:
+            embedded_dims.extend(embedding.active_dims)
+            embeddings.append(embedding(x[..., embedding.active_dims]))
+
+        embedded_dims = [
+            dim if dim >= 0 else x.shape[-1] + dim for dim in embedded_dims
+        ]
+        non_embedded_dims = [
+            dim for dim in range(x.shape[-1]) if dim not in embedded_dims
+        ]
+        x_non_embedded = x[..., non_embedded_dims]
+        x = torch.cat((x_non_embedded, *embeddings), dim=-1)
+        out = super().forward(x)
+        return out
