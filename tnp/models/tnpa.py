28e7a5d92b1df33c0d07a4de7d8d7064a39f42e5
Philip Mortimer <philipmortimer99@aol.com>
2025-06-12 09:29:38 +0100

    New commit after ssh gpu went down with bugs

--- tnp/models/tnpa.py
+++ tnp/models/tnpa.py
@@ -20,7 +20,6 @@ class ARTNPEncoder(nn.Module):
         xy_encoder: nn.Module,
         x_encoder: nn.Module = nn.Identity(),
         y_encoder: nn.Module = nn.Identity(),
-        bound_std: bool = False, # Smooths history - note no support for pretraining here at the moment. Doesn't quite support all same function as in tnpa from original codebase.
     ):
         super().__init__()
 
@@ -34,22 +33,8 @@ class ARTNPEncoder(nn.Module):
         "xc: [m, nc, dx]", "yc: [m, nc, dy]", "xt: [m, nt, dx]", "yt: [m, nt, dy]", "return: [m, n, dz]"
     )
     def forward(
-        self, xc: torch.Tensor, yc: torch.Tensor, xt: torch.Tensor, yt: Optional[torch.Tensor] = None
+        self, xc: torch.Tensor, yc: torch.Tensor, xt: torch.Tensor, yt: torch.Tensor, bound_std: bool
     ) -> torch.Tensor:
-        assert ((yt == None) != self.training), "Debug statement ensuring yt matches training mode expectation"
-        # During training we use teacher forcing and have access to yt (train mode).
-        # During evaluation (eval mode) yt is not provided and will be none.
-        if yt is None:
-            print("IMPLEMENT ME") # TODO add
-            exit(0)
-        else:
-            return self._forward_train_pass(xc, yc, xt, yt)
-
-    def _predict():
-        
-
-    # Does a single training pass using teacher forcing
-    def _forward_train_pass(self, xc, yc, xt, yt):
         m = xc.shape[0]
         # Preprocesses observations
         yc = torch.cat((yc, torch.zeros(yc.shape[:-1] + (1,)).to(yc)), dim=-1)
@@ -65,8 +50,8 @@ class ARTNPEncoder(nn.Module):
         yc_encoded, yt_encoded = y_encoded.split((yc.shape[1], yt.shape[1]), dim=1)
         
         # Concats ctx with fake and real target points
-        inp = self._construct_input(xc_encoded, yc_encoded, xt_encoded, yt_encoded)
-        mask, num_tar = self._create_mask(num_ctx=xc.shape[1], num_tar=xt.shape[1])
+        inp = self._construct_input(xc_encoded, yc_encoded, xt_encoded, yt_encoded, bound_std)
+        mask, num_tar = self._create_mask(num_ctx=xc.shape[1], num_tar=xt.shape[1], device=xt.device)
         mask = mask.unsqueeze(0).expand(m, -1, -1) # [m, nc + 2*nt, nc+2*nt]  Broadcast mask to batch
 
         # Embeds data and runs through transformer encoder
@@ -75,10 +60,11 @@ class ARTNPEncoder(nn.Module):
 
         return out[:, -num_tar:]
 
-    def _construct_input(self, xc, yc, xt, yt):
+
+    def _construct_input(self, xc: torch.Tensor, yc: torch.Tensor, xt: torch.Tensor, yt: torch.Tensor, bound_std: bool):
         x_y_ctx = torch.cat((xc, yc), dim=-1)
         x_0_tar = torch.cat((xt, torch.zeros_like(yt)), dim=-1)
-        if self.training and self.bound_std:
+        if self.training and bound_std:
             yt_noise = yt + 0.05 * torch.randn_like(yt) # add noise to the past to smooth the model
             x_y_tar = torch.cat((xt, yt_noise), dim=-1)
         else:
@@ -86,9 +72,9 @@ class ARTNPEncoder(nn.Module):
         inp = torch.cat((x_y_ctx, x_y_tar, x_0_tar), dim=1) # [m, nc + 2*nt, dx + dy + 1] (probably - depends on encoders etc)
         return inp
 
-    def _create_mask(self, num_ctx, num_tar):
+    def _create_mask(self, num_ctx, num_tar, device):
         num_all = num_ctx + num_tar
-        mask = torch.zeros((num_all+num_tar, num_all+num_tar), device='cuda').fill_(float('-inf'))
+        mask = torch.zeros((num_all+num_tar, num_all+num_tar), device=device).fill_(float('-inf'))
         mask[:, :num_ctx] = 0.0 # all points attend to context points
         mask[num_ctx:num_all, num_ctx:num_all].triu_(diagonal=1) # each real target point attends to itself and precedding real target points
         mask[num_all:, num_ctx:num_all].triu_(diagonal=0) # each fake target point attends to preceeding real target points
