cc9724eb7fde140c0135a544a0821a45521dcb26
Philip Mortimer <philipmortimer99@aol.com>
2025-06-21 12:34:14 +0100

    Loss update

--- tnp/utils/np_functions.py
+++ tnp/utils/np_functions.py
@@ -51,7 +51,23 @@ def np_loss_fn(
     Returns:
         loss: average negative log likelihood.
     """
-    pred_dist = np_pred_fn(model, batch, num_samples)
+    
+    # BatchedCausalTNP uses different loss factorisation - loss is computed by weighting loss for each point AR style
+    if isinstance(model, BatchedCausalTNP):
+        pred_dist = np_pred_fn(model, batch, num_samples)
+        m, N, dy = batch.y.shape
+        log_p = pred_dist.log_prob(batch.y) # [m, N, dy]
+        print("-----")
+        print(batch.y.shape)
+        print(log_p.shape)
+        print("----")
+        assert log_p.shape == batch.y.shape, "Incorrect dims for loss"
+        log_p = log_p.mean(-1) # [m, N] - takes mean over dy dimension
+        mask = torch.ones((m, N), device=log_p.device) # Can use this specify what dims we care about / don't
+        mask[:,0] = 0.0 # We don't care about zero shot prediction. Could optimise and remove this from being predicted by decoder but may be an interesting prior?
+        # We average over all valid tokens in the calculation (and normalise by length)
+        nll = - (log_p * mask).sum() / mask.sum()
+    
+    pred_dist = np_pred_fn(model, batch, num_samples)  
     loglik = pred_dist.log_prob(batch.yt).sum() / batch.yt[..., 0].numel()
-
     return -loglik
