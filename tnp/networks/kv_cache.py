402d7ba1f0e0e65f12edcfaad6bcca221636def9
Philip Mortimer <philipmortimer99@aol.com>
2025-06-25 20:15:09 +0100

    Changed id

--- tnp/networks/kv_cache.py
+++ tnp/networks/kv_cache.py
@@ -2,10 +2,10 @@
 from typing import Optional
 import torch
 
-def update_kv_cache(k_new, v_new, cache: Optional[dict], id):
+def update_kv_cache(k_new, v_new, cache: Optional[dict], cache_id):
     if cache is None: return k_new, v_new # Training - in case of an empty cache, k and v are simply returned
 
-    k_curr, v_curr = cache.get(id, (None, None)) # Gets previously cached k and v values (or (None, None) if layer has not been cached yet)
+    k_curr, v_curr = cache.get(cache_id, (None, None)) # Gets previously cached k and v values (or (None, None) if layer has not been cached yet)
     
     if k_curr is None: 
         k_updated, v_updated = k_new, v_new # Layer has not yet been cached (first item in seq) so return k and v
@@ -13,6 +13,6 @@ def update_kv_cache(k_new, v_new, cache: Optional[dict], id):
         # Adds k and v to cache history
         k_updated = torch.cat((k_curr, k_new), dim=2) #[B, H, L, Dk]
         v_updated = torch.cat((v_curr, v_new), dim=2) #[B, H, L, Dv]
-    cache[id] = (k_updated, v_updated) # Updates cache
+    cache[cache_id] = (k_updated, v_updated) # Updates cache
     return k_updated, v_updated
 
