03fd9b8bfc39fba82294201d58a19c4bcda51d58
Philip Mortimer <philipmortimer99@aol.com>
2025-06-30 21:02:45 +0100

    graphs

--- experiments/kv_cache_plots.py
+++ experiments/kv_cache_plots.py
@@ -133,15 +133,183 @@ def measure_condition_time_memory_kv():
     fig, ax = plt.subplots(figsize=(7, 5))
     ax.plot(upper_ctxs, memory)
     ax.set_xlabel('Context Size')
-    ax.set_ylabel('Cumulaitve Memory Usage (MB)')
+    ax.set_ylabel('Cumulative Memory Usage (MB)')
     ax.set_title('Memory Use with KV-Caching')
     ax.grid(True, linestyle='--', alpha=0.4)
     fig.tight_layout()
-    plt.savefig('experiments/plot_results/kv/memory_vs_ctx.png', dpi=300)         
+    plt.savefig('experiments/plot_results/kv/memory_vs_ctx.png', dpi=300)      
+  
 
+# Measures the conditioning time for the model
+@torch.no_grad
+def compare_kv_against_none(strategy="fixed"):
+    assert strategy in {"fixed", "scale"}, "Invalid strategy"
+    # Gets model
+    device='cuda'
+    model = get_model('experiments/configs/synthetic1dRBF/gp_priorbatched_causal_tnp_rbf_rangesame.yml', 'pm846-university-of-cambridge/mask-priorbatched-tnp-rbf-rangesame/model-smgj3gn6:v200', device=device)
+    model.eval()
+    # Dataset
+    burn_in = 1 # Number of burn in runs to ignore
+    aggregate_over = 2 # Number of runs to aggregate data over
+    token_step = 500 # How many increments of tokens to go up in
+    max_nc, dx, dy, m = 50_000, 1, 1, 1
+    nt = 128 if strategy == "fixed" else max_nc
+    max_high = 2
+    xcs = (torch.rand((1, max_nc, dx), device=device) * max_high * 2) - max_high
+    ycs = (torch.rand((1, max_nc, dy), device=device) * max_high * 2) - max_high
+    xts = (torch.rand((1, nt, dx), device=device) * max_high * 2) - max_high
+    #yts = (torch.rand((1, nt, dy), device=device) * max_high * 2) - max_high
+    # Results structures
+    context_sizes = np.arange(start=0, stop=max_nc, step=token_step, dtype=int)
+    upper_ctxs = np.array([min(i + token_step, max_nc) for i in context_sizes])
+    runtime_no_kv = np.zeros((aggregate_over, len(context_sizes)))
+    condition_kv = np.zeros((aggregate_over, len(context_sizes)))
+    query_kv = np.zeros((aggregate_over, len(context_sizes)))
+    memory_kv = np.zeros((aggregate_over, len(context_sizes)))
+    memory_no_kv = np.zeros((aggregate_over, len(context_sizes)))
+    ctx_inc = 0
+    # Measures memory and runtime for model with no KV cache
+    starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+    for j in range(burn_in + aggregate_over):
+        # Initailises  KV cache
+        start_token = model.encoder.empty_token.expand(m, -1, -1) # Starts with empty token (prior condition)
+        kv_cache = init_kv_cache()
+        model.encoder.update_context(start_token, kv_cache)
+        torch.cuda.reset_peak_memory_stats() #  Resets memory stats - we want cumulative memory
+        # Adds context tokens n at a time
+        ctx_inc = 0
+        for lower_ctx, upper_ctx in zip(context_sizes, upper_ctxs):
+            xc = xcs[:, :upper_ctx, :]
+            yc = ycs[:, :upper_ctx, :]
+            if strategy == "fixed": xt = xts
+            else: xt = xts[:,:upper_ctx,:]
 
+            # Sets up measures
+            torch.cuda.synchronize()
+            starter.record()
+            # Core update step
+            with torch.no_grad():
+                pred_dist = model.likelihood(model.decoder(model.encoder(xc=xc, yc=yc, xt=xt), xt))
+            # Measures time and memory
+            ender.record()
+            torch.cuda.synchronize()
+            peak_memory_mb = torch.cuda.max_memory_allocated() / (1024 * 1024)
+            update_time_ms = starter.elapsed_time(ender)
+
+            # Writes measured results
+            if j >= burn_in:
+                write_j = j - burn_in
+                runtime_no_kv[write_j, ctx_inc] = update_time_ms
+                memory_no_kv[write_j, ctx_inc] = peak_memory_mb
+            ctx_inc += 1
+
+    # Measures KV caching performance
+    for j in range(burn_in + aggregate_over):
+        # Initailises  KV cache
+        start_token = model.encoder.empty_token.expand(m, -1, -1) # Starts with empty token (prior condition)
+        kv_cache = init_kv_cache()
+        model.encoder.update_context(start_token, kv_cache)
+        torch.cuda.reset_peak_memory_stats() #  Resets memory stats - we want cumulative memory
+        # Adds context tokens n at a time
+        ctx_inc = 0
+        for lower_ctx, upper_ctx in zip(context_sizes, upper_ctxs):
+            xc_new = xcs[:, lower_ctx:upper_ctx, :]
+            yc_new = ycs[:, lower_ctx:upper_ctx, :]
+            if strategy == "fixed": xt = xts
+            else: xt = xts[:,:upper_ctx,:]
+
+            # Measures condition step
+            torch.cuda.synchronize()
+            starter.record()
+            # Core update step
+            with torch.no_grad():
+                new_zc = model.encoder._preprocess_context(xc_new, yc_new)
+                model.encoder.update_context(new_zc, kv_cache)
+            # Measures time and memory
+            ender.record()
+            torch.cuda.synchronize()
+            condition_time_ms = starter.elapsed_time(ender)
+
+            # Measures query step
+            torch.cuda.synchronize()
+            starter.record()
+            # Core update step
+            with torch.no_grad():
+                zt = model.encoder._preprocess_targets(xt, dy)
+                pred_dist = model.likelihood(model.decoder(model.encoder.query(zt, kv_cache), xt))
+            # Measures time and memory
+            ender.record()
+            torch.cuda.synchronize()
+            query_time_ms = starter.elapsed_time(ender)
+
+            peak_memory_mb = torch.cuda.max_memory_allocated() / (1024 * 1024)
+            # Writes measured results
+            if j >= burn_in:
+                write_j = j - burn_in
+                condition_kv[write_j, ctx_inc] = condition_time_ms
+                query_kv[write_j, ctx_inc] = query_time_ms
+                memory_kv[write_j, ctx_inc] = peak_memory_mb
+            ctx_inc += 1
+
+    # Aggregates results
+    runtime_no_kv = np.mean(runtime_no_kv, axis=0)
+    condition_kv = np.mean(condition_kv, axis=0)
+    query_kv = np.mean(query_kv, axis=0)
+    memory_kv = np.mean(memory_kv, axis=0)
+    memory_no_kv = np.mean(memory_no_kv, axis=0)
+    runtime_kv = condition_kv + query_kv
+
+    # Writes results to file
+    summary_block = f"""
+    ----------------------------
+    Cumulative Context Size: {upper_ctxs}
+    Runtime Incremental no KV (ms): {runtime_no_kv}
+    Query Runtime Incremental KV (ms): {query_kv}
+    Condition Runtime Incremental KV (ms): {condition_kv}
+    Runtime Incremental KV (ms): {runtime_kv}
+    Memory Cumulative KV (Mb): {memory_kv}
+    Memory Cumulative No KV (Mb): {memory_no_kv}
+    """
+    print(summary_block)
+    with open('experiments/plot_results/kv/' + 'comparison_kv_vs_none.txt', 'w') as file_object:
+        file_object.write(summary_block)
+
+    # Plots runtime results
+    runtime_file_name = f'experiments/plot_results/kv/kv_without_runtime_{strategy}.png'
+    fig, ax = plt.subplots(figsize=(7, 5))
+    ax.plot(upper_ctxs, runtime_no_kv, label='No Caching')
+    ax.plot(upper_ctxs, runtime_kv, label='KV Caching (Condition + Query)')
+    ax.plot(upper_ctxs, condition_kv, label='Condition (KV Caching)', linestyle="dashed")
+    ax.plot(upper_ctxs, query_kv, label='Query (KV Caching)', linestyle="dashed")
+    ax.set_xlabel('Context Size')
+    ax.set_ylabel('Runtime (ms)')
+    ax.legend()
+    tit_text = f'NT={nt}' if strategy == "fixed" else 'NT=NC'
+    ax.set_title(f'Runtime as Context Size Increases ({tit_text})')
+    ax.grid(True, linestyle='--', alpha=0.4)
+    fig.tight_layout()
+    plt.savefig(runtime_file_name, dpi=300)
+
+    # Plots memory
+    memory_file_name = f'experiments/plot_results/kv/kv_without_memory_{strategy}.png'
+    fig, ax = plt.subplots(figsize=(7, 5))
+    ax.plot(upper_ctxs, memory_kv, label='No Caching')
+    ax.plot(upper_ctxs, memory_no_kv, label='KV Caching (Condition + Query)')
+    ax.set_xlabel('Context Size')
+    ax.set_ylabel('Cumulative Memory Usage (MB)')
+    ax.set_title(f'Memory Usage as Context Size Increases ({tit_text})')
+    ax.legend()
+    tit_text = f'NT={nt}' if strategy == "fixed" else 'NT=NC'
+    ax.set_title(f'Runtime as Context Size Increases ({tit_text})')
+    ax.grid(True, linestyle='--', alpha=0.4)
+    fig.tight_layout()
+    plt.savefig(memory_file_name, dpi=300)
+
+    
 
 
 if __name__ == "__main__":
     #test_kv_cache()
-    measure_condition_time_memory_kv()
+    #measure_condition_time_memory_kv()
+    compare_kv_against_none(strategy="fixed")
+    compare_kv_against_none(strategy="scale")
