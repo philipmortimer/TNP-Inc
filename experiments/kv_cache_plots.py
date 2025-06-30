1fb1b6b0ee1c9a5e7c6b40d512518ab4acb11752
Philip Mortimer <philipmortimer99@aol.com>
2025-06-30 17:37:47 +0100

    Data gathering KV

--- experiments/kv_cache_plots.py
+++ experiments/kv_cache_plots.py
@@ -57,59 +57,69 @@ def measure_condition_time_memory_kv():
     # Dataset
     burn_in = 1 # Number of burn in runs to ignore
     aggregate_over = 2 # Number of runs to aggregate data over
-    token_step = 1 # How many increments of tokens to go up in
-    max_nc, dx, dy, m = 1000, 1, 1, 1
-    xcs = torch.randn((1, max_nc, dx), device=device)
-    ycs = torch.randn((1, max_nc, dy), device=device)
+    token_step = 50 # How many increments of tokens to go up in
+    max_nc, dx, dy, m = 10_000, 1, 1, 1
+    max_high = 2
+    xcs = (torch.rand((1, max_nc, dx), device=device) * max_high * 2) - max_high
+    ycs = (torch.rand((1, max_nc, dy), device=device) * max_high * 2) - max_high
     # Results structures
     context_sizes = np.arange(start=0, stop=max_nc, step=token_step, dtype=int)
-    no_ctx_points = len(context_sizes)
-    runtime = np.zeros((aggregate_over, no_ctx_points))
-    memory = np.zeros((aggregate_over, no_ctx_points))
+    upper_ctxs = np.array([min(i + token_step, max_nc) for i in context_sizes])
+    runtime = np.zeros((aggregate_over, len(context_sizes)))
+    memory = np.zeros((aggregate_over, len(context_sizes)))
     ctx_inc = 0
+    starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
     for j in range(burn_in + aggregate_over):
         # Initailises  KV cache
         start_token = model.encoder.empty_token.expand(m, -1, -1) # Starts with empty token (prior condition)
         kv_cache = init_kv_cache()
         model.encoder.update_context(start_token, kv_cache)
+        torch.cuda.reset_peak_memory_stats() #  Resets memory stats - we want cumulative memory
         # Adds context tokens n at a time
         ctx_inc = 0
-        for i in context_sizes:
-            up_lim = context_sizes[i + 1] if i  + 1 < len(context_sizes) else context_sizes[-1]
-            xc_new = xcs[:, i:up_lim, :]
-            yc_new = ycs[:, i:up_lim, :]
+        for lower_ctx, upper_ctx in zip(context_sizes, upper_ctxs):
+            xc_new = xcs[:, lower_ctx:upper_ctx, :]
+            yc_new = ycs[:, lower_ctx:upper_ctx, :]
 
             # Sets up measures
             torch.cuda.synchronize()
-            torch.cuda.reset_peak_memory_stats()
-            start = time.perf_counter()
+            starter.record()
             # Core update step
             with torch.no_grad():
                 new_zc = model.encoder._preprocess_context(xc_new, yc_new)
                 model.encoder.update_context(new_zc, kv_cache)
             # Measures time and memory
+            ender.record()
             torch.cuda.synchronize()
-            end = time.perf_counter()
             peak_memory_mb = torch.cuda.max_memory_allocated() / (1024 * 1024)
-            update_time = (end - start) * 1000
+            update_time_ms = starter.elapsed_time(ender)
 
             # Writes measured results
             if j >= burn_in:
                 write_j = j - burn_in
-                runtime[write_j, ctx_inc] = update_time
+                runtime[write_j, ctx_inc] = update_time_ms
                 memory[write_j, ctx_inc] = peak_memory_mb
             ctx_inc += 1
     # Averages runtime and memory
     runtime = np.mean(runtime, axis=0)
     memory = np.mean(memory, axis=0)
 
-    print(runtime)
-    print(memory)
+    # Writes results to file
+    summary_block = f"""
+    ----------------------------
+    Cumulative Context Size: {upper_ctxs}
+    Runtime Incremental (ms): {runtime}
+    Memory Cumulative (Mb): {memory}
+    """
+    print(summary_block)
+    with open('experiments/plot_results/kv/' + 'mem_run.txt', 'w') as file_object:
+        file_object.write(summary_block)
+    
             
 
 
 
 
 if __name__ == "__main__":
-    test_kv_cache()
+    #test_kv_cache()
     measure_condition_time_memory_kv()
