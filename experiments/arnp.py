1b4cf9c182123b25bb02db0279c1211e6dc0527e
Philip Mortimer <philipmortimer99@aol.com>
2025-07-09 18:20:47 +0100

    Part way through predict code

--- experiments/arnp.py
+++ experiments/arnp.py
@@ -4,7 +4,7 @@
 import torch
 from check_shapes import check_shapes
 from torch import nn
-from typing import Optional, Union, Literal
+from typing import Optional, Union, Literal, Callable
 from tnp.utils.np_functions import np_pred_fn
 from tnp.data.base import Batch
 from plot_adversarial_perms import get_model
@@ -19,27 +19,31 @@ import numpy as np
     "xc: [m, nc, dx]", "yc: [m, nc, dy]", "xt: [m, nt, dx]", "yt: [m, nt, dy]",
 )
 @torch.no_grad
-def _shuffle_targets(np_model: nn.Module, xc: torch.Tensor, yc: torch.Tensor, xt: torch.Tensor, yt: torch.Tensor,
+def _shuffle_targets(np_model: nn.Module, xc: torch.Tensor, yc: torch.Tensor, xt: torch.Tensor, yt: Optional[torch.Tensor],
     order: Literal["random", "given", "left-to-right", "variance"]):
     m, nt, dx = xt.shape
-    _, _, dy = yt.shape
+    _, _, dy = yc.shape
     device = xt.device
     if order == "given":
         return xt, yt
     elif order == "random":
         perm = torch.rand(m, nt, device=device).argsort(dim=1)
         perm_x = perm.unsqueeze(-1).expand(-1, -1, dx)
-        perm_y = perm.unsqueeze(-1).expand(-1, -1, dy)
         xt_shuffled = torch.gather(xt, 1, perm_x)
-        yt_shuffled = torch.gather(yt, 1, perm_y)
+        if yt is not None:
+            perm_y = perm.unsqueeze(-1).expand(-1, -1, dy)
+            yt_shuffled = torch.gather(yt, 1, perm_y)
+        else: yt_shuffled = None
         return xt_shuffled, yt_shuffled
     elif order == "left-to-right":
         assert dx == 1, "left-to-right ordering only supported for one dimensional dx"
         perm = torch.argsort(xt.squeeze(-1), dim=1)
         perm_x = perm.unsqueeze(-1).expand(-1, -1, dx)
-        perm_y = perm.unsqueeze(-1).expand(-1, -1, dy)
         xt_sorted = torch.gather(xt, 1, perm_x)
-        yt_sorted = torch.gather(yt, 1, perm_y)
+        if yt is not None:
+            perm_y = perm.unsqueeze(-1).expand(-1, -1, dy)
+            yt_sorted = torch.gather(yt, 1, perm_y)
+        else: yt_sorted = None
         return xt_sorted, yt_sorted
     elif order == "variance":
         # Predicts all target points conditioned on context points and orders (highest variance first) - this is obviously much more expensive
@@ -48,9 +52,11 @@ def _shuffle_targets(np_model: nn.Module, xc: torch.Tensor, yc: torch.Tensor, xt
         var = pred_dist.variance.mean(-1) # Gets variance (averaged over dy) [m, nt]
         perm = torch.argsort(var, dim=1, descending=True)
         perm_x = perm.unsqueeze(-1).expand(-1, -1, dx)
-        perm_y = perm.unsqueeze(-1).expand(-1, -1, dy)
         xt_sorted = torch.gather(xt, 1, perm_x)
-        yt_sorted = torch.gather(yt, 1, perm_y)
+        if yt is not None:
+            perm_y = perm.unsqueeze(-1).expand(-1, -1, dy)
+            yt_sorted = torch.gather(yt, 1, perm_y)
+        else: yt_sorted = None
         return xt_sorted, yt_sorted
 
 
@@ -62,6 +68,7 @@ def _shuffle_targets(np_model: nn.Module, xc: torch.Tensor, yc: torch.Tensor, xt
 def ar_loglik(np_model: nn.Module, xc: torch.Tensor, yc: torch.Tensor, xt: torch.Tensor, yt: torch.Tensor,
     normalise: bool = True, order: Literal["random", "given", "left-to-right", "variance"] = "random") -> torch.Tensor:
     xt, yt = _shuffle_targets(np_model, xc, yc, xt, yt, order)
+    np_model.eval()
     m, nt, dx = xt.shape
     _, nc, dy = yc.shape
     log_probs = torch.zeros((m), device=xt.device)
@@ -81,14 +88,72 @@ def ar_loglik(np_model: nn.Module, xc: torch.Tensor, yc: torch.Tensor, xt: torch
     return log_probs
 
 
+UpdateCtxAndQueryTgtFuncs = Tuple[
+    Callable[[int], None] # Initialise function
+    Callable[[torch.Tensor, torch.Tensor], None],  # inc_ctxt_update
+    Callable[[torch.Tensor], td.Distribution]     # query_tgt
+]
+
+
 @check_shapes(
     "xc: [m, nc, dx]", "yc: [m, nc, dy]", "xt: [m, nt, dx]"
 )
 @torch.no_grad
 def ar_predict(model, xc: torch.Tensor, yc: torch.Tensor, xt: torch.Tensor,
-    order: Literal["random", "given", "left-to-right", "variance"] = "random"):
+    order: Literal["random", "given", "left-to-right", "variance"] = "random",
+    num_samples: int = 10,
+    inc_ctx_and_query: Optional[UpdateCtxAndQueryTgtFuncs] = None
+    ):
     m, nt, dx = xt.shape
     _, nc, dy = yc.shape
+    device = xt.device
+    xt, _ = _shuffle_targets(np_model, xc, yc, xt, None, order) # Should I shuffle before or after stacking?
+
+    xc_stacked = _stack(xc, num_samples=num_samples, dim=0)
+    yc_stacked = _stack(yc, num_samples=num_samples, dim=0)
+    xt_stacked = _stack(xt, num_samples=num_samples, dim=0)
+    yt_preds_mean, yt_preds_std = torch.empty((m * num_samples, nt, dy), device=device), torch.empty((m * num_samples, nt, dy), device=device)
+
+    if inc_ctx_and_query is not None:
+        init_func, inc_update, query = inc_ctx_and_query
+        init_func(nc + nt)
+        inc_update(xc_stacked, yc_stacked)
+
+    for i in range(nt):
+        xt_tmp = xt_stacked[:, i:i+1,:]
+        if inc_ctx_and_query is None:
+            batch = Batch(xc=xc_stacked, yc=yc_stacked, xt=xt_tmp, yt=None, x=None, y=None)
+            pred_dist = np_pred_fn(model, batch)
+        else:
+            pred_dist = query(xt_tmp)
+        assert isinstance(pred_dist, td.Normal), "Must predict a gaussian"
+        pred_mean, pred_std = pred_dist.mean, pred_dist.stddev
+        yt_preds_mean[:,i:i+1,:] = pred_mean
+        yt_preds_std[:,i:i+1,:] = pred_std
+        # Samples from the predictive distribution and updates the context
+        if i < nt:
+            yt_sampled = pred_dist.sample() # [m * num_samples, 1, dy]
+            if inc_ctx_and_query is None:
+                xc_stacked = torch.cat((xc_stacked, xt_tmp), dim=1)
+                yc_stacked = torch.cat((yc_stacked, yt_sampled), dim=1)
+            else:
+                inc_update(xt_tmp, yt_sampled)
+    # TODO: Check where to permute, remove noise from samples and fix bugs
+    # Returns a mixture of gaussians of all the predicted distributions
+    yt_preds_mean = yt_preds_mean.view(m, num_samples, nt, dy)
+    yt_preds_std = yt_preds_std.view(m, num_samples, nt, dy)
+    # Permutes to [m, nt, dy, num_samples]
+    yt_preds_mean = yt_preds_mean.permute(1,2,3,0)
+    yt_preds_std  = yt_preds_std.permute(1,2,3,0)
+    mix  = td.Categorical(torch.full((m, nt, dy, num_samples), 1.0 / num_samples, device=device))
+    comp = td.Normal(yt_preds_mean, yt_preds_std)
+    approx_dist = td.MixtureSameFamily(mix, comp)
+    return approx_dist
+
+
+def _stack(x, num_samples, dim):
+    return torch.stack([x] * num_samples, dim=dim)
+
 
 
 
@@ -98,7 +163,7 @@ def ar_predict(model, xc: torch.Tensor, yc: torch.Tensor, xt: torch.Tensor,
 # Compares NP models in AR mode on RBF set
 def compare_rbf_models(base_out_txt_file: str, device: str = "cuda"):
     # Hypers to select - also look at dataset hypers
-    ordering = "variance"
+    ordering = "random"
     # End of hypers
     # List of models to compare
     tnp_plain = ('experiments/configs/synthetic1dRBF/gp_plain_tnp_rangesame.yml',
@@ -149,7 +214,7 @@ def compare_rbf_models(base_out_txt_file: str, device: str = "cuda"):
         mod_sum = ("-" * 20) + f"\nModel: {model_name}\nMean LL: {mean_ll}\n"
         print(mod_sum)
         out_txt += mod_sum
-    with open(out_txt_file + f'_{ordering}.txt', 'w') as file:
+    with open(base_out_txt_file + f'_{ordering}.txt', 'w') as file:
         file.write(out_txt)
 
 
