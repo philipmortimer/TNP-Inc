68ab3d63c278cde34f1b34ed6c4b59a5b48eb121
Philip Mortimer <philipmortimer99@aol.com>
2025-07-15 21:38:07 +0100

    Small

--- experiments/arnp.py
+++ experiments/arnp.py
@@ -273,7 +273,7 @@ def measure_perf_timings():
     burn_in = 1 # Number of burn in runs to ignore
     aggregate_over = 1 # Number of runs to aggregate data over
     token_step = 50 # How many increments of tokens to go up in
-    min_nt, max_nt = 1, 2002
+    min_nt, max_nt = 1, 2003
     dx, dy, m = 1, 1, 1
     nc_start = 1
     num_samples=50 # Samples to unroll in ar_predict
