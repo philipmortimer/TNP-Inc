33145baaafe929bee9f60a47886819045b46bebb
Philip Mortimer <philipmortimer99@aol.com>
2025-07-22 15:07:06 +0100

    Small order change

--- experiments/configs/hadISD/run_long/run_cmd.sh
+++ experiments/configs/hadISD/run_long/run_cmd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script to run a list of commands on gpu one after another for a long amount of time
-COMMAND_FILE="/homes/pm846/Thesis/Code/TNP-Inc/experiments/configs/hadISD/run_long.txt"
+COMMAND_FILE="/homes/pm846/Thesis/Code/TNP-Inc/experiments/configs/hadISD/run_long/commands.txt"
 
 # Does command file actually exist
 if [ ! -f "$COMMAND_FILE" ]; then
